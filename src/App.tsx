3ec72be46da3bf8f08ab546e7482d4c821015acc
MariamMohammed99 <mariammamin19@gmail.com>
2024-09-03 19:24:39 +0300

    feat: routing added

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import LandingDisplayPage from './pages/LandingDisplay';
-
-const App = () => {
-  return <LandingDisplayPage></LandingDisplayPage>;
-};
-
-export default App;
---
+++ src/pages/CityWeatherDashboard/index.tsx
@@ -0,0 +1,5 @@
+const CityWeatherDashboardPage = () => {
+  return <div>CityWeatherDashboardPage</div>;
+};
+
+export default CityWeatherDashboardPage;
