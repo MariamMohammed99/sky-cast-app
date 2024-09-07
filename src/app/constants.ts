5ce05f16b42985c90b9b69243a96fc980f1bcccf
MariamMohammed99 <mariammamin19@gmail.com>
2024-09-07 22:01:22 +0300

    fix: vite config

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -15,7 +15,7 @@ export const HOURLY_TIME_INTERVAL = 1;
 
 //#region location api
 
-export const LOCATION_BASE_URL = '/geo/v1/geo/';
+export const LOCATION_BASE_URL = 'http://geodb-free-service.wirefreethought.com/v1/geo/';
 export const GET_CITY_NAME_URL = 'locations/{{lat+long}}/nearbyCities';
 export const SEARCH_CITY_URL = 'cities';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default {
   plugins: [react()],
-  server: {
-    proxy: {
-      '/geo': {
-        target: 'http://geodb-free-service.wirefreethought.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/geo/, ''),
-        secure: false, // For HTTP, disable SSL verification
-      },
-    },
-  },
-});
+};
