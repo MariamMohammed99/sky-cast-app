708bc051553e79be06a068b992ee79c105da4812
MariamMohammed99 <mariammamin19@gmail.com>
2024-09-07 21:57:28 +0300

    feat: add https proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
-};
+  server: {
+    proxy: {
+      '/geo': {
+        target: 'http://geodb-free-service.wirefreethought.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/geo/, ''),
+        secure: false, // For HTTP, disable SSL verification
+      },
+    },
+  },
+});
